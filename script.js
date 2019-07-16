291bb8f486181aac8fb0b916dcfafca18aaad83e
areeshatehreem <50449369+areeshatehreem@users.noreply.github.com>
2019-07-16 15:54:02 +0500

    wip

---
+++ script.js
@@ -0,0 +1,1036 @@
+let profession = ["doctor", "engineer", "astronomers"]
+
+
+
+let doctor = {
+
+    profession: "dentist",
+    salary: 215521,
+    houseJob: "eightYear",
+    isAnyScope: true,
+    yearsToBe: 4,
+    famousUniversity: [
+        "dowInternationalDentalCollege",
+        "bahriaUniversityMedicalAndDentalCollege"
+    ],
+    skills: [
+        "Good practical skills.",
+        "Ability to work long hours, often under pressure.",
+        "Analytical ability"
+    ]
+
+}
+
+let physiotherapist = {
+
+    profession: "Physiotherapist",
+    fees: 80000,
+    houseJob: " oneYear",
+    yearsToBe: 4,
+    yearsInDptProgram: 3,
+    salaryInMounth: 500,
+    salaryInYear: 497412,
+    isAnyScope: true,
+    famousUniversity: [
+        " liaquatNationalMedicalCollege",
+        " baqaiUniversity"
+    ]
+}
+
+
+let dermatology = {
+    work: "treatmentOfDiseasesOfTheSkin,HairAndNails.",
+    scope: "moreDemanded",
+    salary: 246112,
+    jobs: [
+        "clicnic",
+        "hospitals",
+    ],
+    skill: "communicationSkills",
+    University: [
+        "agaKhanUniversity",
+        "dowUniversityOfHealthSciences",
+    ]
+
+}
+
+
+let diagnosticRadiology = {
+
+    profession: "DiagnosticRadiology",
+    houseJob: " severalYearOfTraining",
+    yearsToBe: 4,
+    salaryInYear: 456712,
+    isAnyScope: true,
+    famousUniversity: [
+        " agaKhanUniversity",
+        " dowUniversityOfHealthSciences"
+    ],
+    work: "treatingDiseaseAndInjury,UsingMedicalImagingTechniquesSuchAsMRI,CT",
+
+
+
+}
+
+
+
+let anesthesiology = {
+
+    profession: "Anesthesiology",
+    houseJobInYears: 2,
+    yearsToBe: 4,
+    salaryInYear: 497412,
+    isAnyScope: true,
+    famousUniversity: [
+        " universityOfKarachi",
+        " universityOfKarachi",
+    ]
+}
+
+
+
+
+let chemicalEngineer = {
+
+    profession: "chemicalEngineer",
+    yearsToBe: 4,
+    salaryInYear: 73294,
+    work: "chemicalEngineersDevelopAndDesignChemicalManufacturingprocesses.",
+    isGoodCareer: true,
+    kindsOfJob: "paper,petroChemicals,FoodProcessing,SpecialtyChemicals,",
+    famousUniversity: [
+        " dawoodUniversityOfEngineering&Technology",
+        "nedUniversityOfEngineeringAndTechnology",
+    ]
+}
+
+
+
+
+
+let mechanicalEngineer = {
+
+    profession: "mechanicalEngineer",
+    yearsToBe: 4,
+    salaryInYear: 43789,
+    isGoodCareer: true,
+    famousUniversity: [
+        " dawoodUniversityOfEngineering&Technology",
+        " nedUniversityOfEngineeringAndTechnology",
+    ]
+}
+
+
+
+
+
+
+let civilEngineer = {
+
+    profession: "civilEngineer",
+    yearsToBe: 4,
+    salaryInYear: 59494,
+    isGoodCareer: true,
+    famousUniversity: [
+        "mehranUniversityOfEngineering&Technology,Jamshoro",
+        "nedUniversityOfEngineeringAndTechnology",
+
+    ]
+}
+
+let aeronauticalEngineer = {
+
+    profession: "aeronauticalEngineer",
+    yearsToBe: 4,
+    salaryInYear: 160290,
+    isGoodCareer: true,
+    isEnjoable: true,
+    work: "AeronauticalEngineersWorkWithAircraft.",
+    famousUniversity: [
+        "nationalUniversityOfSciences&Technology(NUST),Islamabad.",
+        "pakistanInstituteOfEngineeringAndAppliedSciences(PIEAS) ",
+
+    ]
+}
+
+
+
+let electricalEngineer = {
+
+    profession: "electricalEngineer",
+    yearsToBe: 4,
+    salaryInYear: 594952,
+    isGoodCareer: true,
+    famousUniversity: [
+        "nationalUniversityOfSciences&Technology(NUST),Islamabad.",
+        "pakistanInstituteOfEngineeringAndAppliedSciences(PIEAS) ",
+
+
+
+
+    ]
+}
+
+
+
+let accountant = {
+
+    profession: "accountant",
+    yearsToBe: 4.5,
+    salaryInYear: 60000,
+    isGoodCareer: true,
+    scope: "isOnlySocpeInPakistan",
+    IsTtDifficultToPassCA: true,
+    work: "takeInformationAboutFiniancialRecords",
+    famousUniversity: [
+        "charteredAccountant(CA)Colleges&universitiesInPakistan",
+
+    ]
+}
+
+
+
+let fisherMan = {
+
+    profession: "fisherMan",
+    salaryInYear: 3000,
+    isGoodCareer: false,
+    work: "catchingFishes",
+
+}
+
+
+let teacher = {
+
+    profession: "teacher",
+    salaryInYear: undefined,
+    isGoodCareer: true,
+    subjects: undefined,
+    famousUniversity: undefined,
+
+
+
+}
+
+
+
+let architect = {
+
+    profession: "Architect",
+    yearsToBe: 4,
+    payScaleInYear: 1500000,
+    isGoodCareer: true,
+    work: "design,PlanAndDevelop.",
+    famousUniversity: [
+        "indusValleySchoolOfArtAndArtitecture",
+    ]
+}
+
+
+let astronomers = {
+
+    profession: "astronomers",
+    yearsToBe: 4,
+    salaryInYear: 101630,
+    isGoodCareer: true,
+    work: "researchAboutUniverse",
+    famousUniversity: [
+        "instituteOfSpaceAndPlanetarySciences(ISPA),UniversityOfKarachi",
+        "departmentOfSpaceScience,UniversityOfPunjab.",
+
+    ]
+}
+
+
+
+let softwareEngineer = {
+
+    profession: "softwareEngineer",
+    yearsToBe: 4,
+    salaryInYear: 59494,
+    isGoodCareer: true,
+    famousUniversity: [
+        "usmanInstituteOfTechnology",
+        "nedUniversityOfEngineeringAndTechnology",
+
+    ]
+}
+
+
+
+let pilot = {
+
+    profession: "pilot",
+    yearsToBe: 2,
+    salaryInYear: 100000,
+    isGoodCareer: true,
+    isItHardToBeAPilot: false,
+    famousUniversity: [
+        "karachiAeroClub",
+        "kkAviationLtd",
+
+    ]
+}
+
+
+
+let fighterPilot = {
+
+    profession: "fighterPilot",
+    yearsToBe: 18 - 34,
+    salaryInYear: 90000,
+    isGoodCareer: true,
+    isItsTraingHard: true,
+    famousUniversity: [
+        "sapsAviationCollege",
+        "pakistanAirForceAcademy",
+
+    ]
+}
+
+
+let banker = {
+
+    profession: "banker",
+    yearsToBe: 4,
+    salaryInYear: 50000,
+    isGoodCareer: true,
+    famousUniversity: [
+        "karachiUniversity",
+        "bahriaUniversity"
+
+    ]
+}
+
+
+
+let lawyer = {
+
+    profession: "lawyer",
+    yearsToBe: 4,
+    salaryInYear: 80000,
+    work: "solveTheCaseUsingKnowledgeOfLegalPrecedents.",
+    degree: " juridicalScience,masterOfLaw",
+    isGoodCareer: true,
+    famousUniversity: [
+        "shaheedZulfiqarAliBhuttoUniversityOfLaw",
+        "sindhMuslimGovernmentLawCollege",
+
+    ]
+}
+
+
+
+let cheif = {
+
+    profession: "cheif",
+    yearsToBe: 2,
+    work: "cookingNewDishes",
+    salaryInYear: 39000,
+    isGoodCareer: true,
+    famousUniversity: [
+        "indusValleySchoolOfArtAndArchitecture",
+        "CMA - CollegeOfMedia&Arts",
+
+    ]
+}
+
+
+
+
+let nurse = {
+
+    profession: "nurse",
+    years: 1,
+    payScaleInYear: 5500,
+    isGoodCareer: true,
+    isAnyScope: false,
+    work: "takeCareofPatients",
+    nurseBy: "course",
+}
+
+
+let driver = {
+
+    profession: "driver",
+    salaryInYear: undefined,
+    isHaveLisence: true,
+    work: "pickAndDrop",
+    scope: "none",
+    isGoodCareer: false,
+
+
+}
+
+
+
+let painter = {
+
+    profession: "painter",
+    career: "none",
+    salary: undefined,
+    work: "paintOnAnyPlace",
+    isRespectedJob: "none",
+
+
+
+}
+
+
+let labour = {
+
+    profession: "labour",
+    isGoodCareer: false,
+    work: "constructAnyBuilding",
+    isneedUniversity: "none",
+    scope: false,
+
+
+}
+
+
+
+let businessMan = {
+
+    profession: "busineesMAn",
+    yearsToBe: 4,
+    salaryInYear: 100000,
+    isGoodCareer: true,
+    scope: true,
+    famousUniversity: [
+        "karachiUniversity",
+        "nust",
+
+    ]
+}
+
+
+
+let salesMan = {
+
+    profession: "salesMan",
+    salaryInYear: 2500,
+    isGoodCareer: false,
+    work: "toSalesThingsOnShop",
+    isRespestableJob: false,
+
+
+}
+
+
+let scientist = {
+
+    profession: "scientist",
+    yearsToBeInCollege: 4,
+    yearForMaster: 3,
+    salaryInYear: 100000,
+    isGoodCareer: true,
+    isDangerousJob: true,
+    famousUniversity: [
+        "fedralUrduUniversity",
+        "muhammadAliJinnahUniversity",
+
+
+    ],
+    skills: [
+        "Patience",
+        "determination",
+    ]
+}
+
+
+
+let police = {
+
+    profession: "police",
+    payScaleInYear: 4.5,
+    isGoodCareer: true,
+    scope: false,
+    isRespestableJob: " none",
+}
+
+
+
+let magician = {
+
+    profession: "magician",
+    work: "toEntertainOthers",
+    isEnjoyableJob: "maybe",
+    scope: "inMYOpinionNo",
+
+}
+
+
+let carpenter = {
+
+    profession: "carpenter",
+    salaryInYear: undefined,
+    isGoodCareer: false,
+    work: "makeFurnitureAndFixture",
+
+
+
+}
+
+
+let swimmers = {
+
+    profession: "swimmers",
+    work: "swimAndTeachesOther",
+    isDangerous: true,
+
+
+
+}
+
+
+
+let gardner = {
+
+    profession: "gardner",
+    scope: false,
+    work: "toGrowPlants",
+
+}
+
+
+let securityGaurd = {
+
+    profession: "securityGaurd",
+    work: "secureToEachOther",
+    responsiblity: "humansSecurity",
+    payment: 2500,
+    isGoodCareer: false,
+    isHaveGun: true,
+    isHaveGunLicience: true,
+}
+
+
+
+let importer = {
+
+    profession: "importer",
+    work: "importThingsFromDifferentCountry",
+    salaryInYear: 150000,
+    isGoodCareer: true,
+    scope: true,
+    vechicle: "byShipsAndRoad",
+}
+
+
+let vascularSurgeons = {
+
+    profession: "vascularSurgeons",
+    yearsToBe: 4,
+    trainingYear: 7,
+    salaryInYear: 90000,
+    isGoodCareer: true,
+    isHaveDemand: true,
+    famousUniversity: [
+        "dowUniversityOfHealthScience",
+        "liaquatNationalHosiptal",
+
+    ]
+}
+
+
+
+let pharmacist = {
+
+    profession: "pharmacist",
+    yearsToBe: 4,
+    trainingYear: 8,
+    salaryInYear: 70000,
+    isGoodCareer: true,
+    responsibility: "toMakeMedicience",
+    work: "researchForCommingNewMedicience",
+    famousUniversity: [
+        "baqaiuniversity",
+        "dowUniversity",
+
+    ]
+}
+
+
+
+let transportationEngineer = {
+
+    profession: "transportationEngineer",
+    yearsToBe: 6,
+    salaryInYear: 65495,
+    isGoodCareer: true,
+    famousUniversity: [
+        "indusUniversity",
+        "mehranUniversityOfEngineering&Technology,Jamshoro",
+
+    ]
+}
+
+
+
+let agricultureEngineer = {
+
+    profession: "agricultureEngineer",
+    yearsToBe: 4,
+    salaryInYear: 45000,
+    isGoodCareer: true,
+    isdemand: true,
+    isSmallOccpation: true,
+    famousUniversity: [
+        ". universityOfAgriculture",
+        " sindhAgricultureUniversity",
+
+    ]
+}
+
+
+
+let petroleumEngineer = {
+
+    profession: "petroleumEngineer",
+    yearsToBe: 4,
+    payOff: "high",
+    isDifficult: true,
+    isGoodCareer: true,
+    famousUniversity: [
+        "khawajaFareedUniversityOfEngineering&InformationTechnology",
+        "ghaziUniversity",
+
+    ]
+}
+
+
+let exporter = {
+
+    profession: "exporter",
+    work: "exportThingFromCountries",
+    salaryInYear: 67000,
+    isGoodCareer: true,
+    scope: "moreScope",
+}
+
+
+let artificialIntelligence = {
+
+    profession: "ArtificialIntelligence",
+    yearsToBe: 4,
+    salaryInYear: 1700000,
+    isGoodCareer: true,
+    famousUniversity: [
+        "behriaUniversity",
+        "nedUniversityOfEngineeringAndTechnology",
+
+    ]
+}
+
+
+
+let networking = {
+
+    profession: "networking",
+
+    salaryInYear: 56000,
+    isGoodCareer: true,
+    famousUniversity: [
+        "nedUniversityOfEngineeringAndTechnology",
+
+    ]
+}
+
+
+let graphics = {
+
+    profession: "graphics",
+    yearsToBe: 4,
+    salaryInYear: 45000,
+    isGoodCareer: true,
+    famousUniversity: [
+        "nedUniversityOfEngineeringAndTechnology",
+
+    ]
+}
+
+
+
+let Criminologists = {
+
+    profession: "Criminologists",
+    yearsToBe: 4,
+    skills: [
+        "analyzeComplexTextualAndCulturalPhenomenon.",
+        "developCriticalObservationsAndAttentionToDetail.",
+    ],
+    salaryInYear: 56778,
+    fees: 38000,
+    isGoodCareer: true,
+    famousUniversity: [
+        ". Pimsat-InstituteOfHigherEducation",
+        "universityOfKarachi",
+
+    ]
+}
+
+
+
+let skinSurgeon = {
+
+    profession: "skinSurgeon",
+    yearsToBe: 4,
+    salaryInYear: 870000,
+    isGoodCareer: true,
+    work: "toCheckPatintentsOfSkinDisease",
+    famousUniversity: [
+        "dowUniversityOfHealthScience",
+        "hamdardUniversity",
+        "liaquatNationalUniversity",
+
+    ]
+}
+
+
+
+
+let made = {
+
+    profession: "made",
+    salaryInYear: 3000,
+    isGoodCareer: false,
+    work: "cleaningPlace",
+
+
+}
+
+
+
+let brainSurgeon = {
+
+    profession: "brainSurgeon",
+    yearsToBe: 4,
+    salaryInYear: 80000,
+    isGoodCareer: true,
+    work: "operationOfBrain",
+    famousUniversity: [
+        "dowUniversityOfHealthScience",
+        "liaquatNationalUniversity",
+        "hamdardUniversity"
+
+    ]
+
+}
+
+
+
+let audiologist = {
+
+    profession: " Audiologist",
+    yearsToBe: 3,
+    salaryInYear: 76000,
+    isGoodCareer: true,
+    scope: "onlyInPakistan",
+    subject: "A-level. We require grades ABB, including a science subject (Biology, Chemistry, Physics, Maths, Psychology or Applied Science).",
+    famousUniversity: [
+        " Isra University ",
+        "Fmh Institute Of Allied Health Sciences",
+        " Fmh College Of Medicine & Dentistry",
+
+    ],
+    skills: "patience and empathy to deal with patients ",
+}
+
+
+
+let guidanceCounsellor = {
+
+    profession: "GuidanceCounsellor",
+    yearsToBe: 4,
+    salaryInYear: 56000,
+    work: [
+        "areer guidance and counseling programs help individuals acquire the knowledge",
+        "mproving decision making skills."
+    ],
+
+    isGoodCareer: true,
+    skills: [
+        "Listening",
+        "Basic empathy",
+    ]
+}
+
+
+
+let dietitian = {
+
+    profession: "Dietitian",
+    yearsToBe: 4,
+    salaryInYear: 67845,
+    isGoodCareer: true,
+    fees: 45000,
+    famousUniversity: [
+        "AgaKhanUniversityHospital",
+        "D.H.A-MedicalCenter",
+
+    ]
+}
+
+
+let psychologists = {
+
+    profession: "Psychologists",
+    salaryInYear: 90000,
+    famousUniversity: [
+        " Muhammad Ali Jinnah University",
+        " Bahria University ( Karachi Campus )",
+
+    ],
+    subject: "A Levels in Biology, Chemistry,Physics, Psychology and Maths ",
+    yearsToBe: 4,
+    isGoodCareer: true,
+    skills: [
+        "Research.",
+        "Numeracy. Although it may not seem intuitive, psychologists must have a facility with numbers",
+    ],
+
+
+
+}
+
+
+let sexologists = {
+
+    profession: "Sexologists",
+    yearsToBe: 4,
+    trainingYear: 2,
+    salaryInYear: 58644,
+    isGoodCareer: true,
+    famousUniversity: [
+        " Aga Khan University Hospital",
+        "  National Medical Centre",
+
+    ],
+    fees: 70000,
+}
+
+
+
+let forestEngineers = {
+
+    profession: "ForestEngineers",
+
+    salaryInYear: 39000,
+    isGoodCareer: true,
+    famousUniversity: [
+        "universityOfKarachi",
+        "nedUniversity",
+
+    ],
+    yearsToBe: 4,
+}
+
+
+
+let postman = {
+
+    profession: "postman",
+    salaryInYear: 3600,
+    isGoodCareer: false,
+    work: "postLetters",
+}
+
+
+
+let fashionDesinger = {
+
+    profession: "fashionDesinger",
+    course: "sewingCloths",
+    salaryInYear: 600000,
+    isGoodCareer: true,
+    famousUniversity: [
+        "idaraHunar-o-danish",
+
+    ],
+    skill: [
+        "sewingNewFashionCloths",
+    ]
+}
+
+
+let mechanic = {
+
+    profession: "mechanic",
+
+    salaryInYear: undefined,
+    isGoodCareer: false,
+    skills: "fastWorking",
+    work: "repairVechicleAndThings",
+}
+
+
+
+let cashier = {
+
+    profession: "cashier",
+    yearsToBe: 4,
+    salaryInYear: 50000,
+    isGoodCareer: true,
+    subject: [
+        "CAA",
+        "ACCA",
+    ],
+    famousUniversity: [
+        "usmanInstituteOfTechnology",
+        "nedUniversity",
+
+    ]
+}
+
+
+
+let cricketer = {
+
+    profession: "cricketer",
+    salaryInYear: 200000,
+    isGoodCareer: true,
+    isGraduated: true,
+    isRespestableJob: true,
+    doPractice: "inFreeTime"
+
+}
+
+
+let footBaller = {
+
+    profession: "footBaller",
+    salaryInYear: 200000,
+    isGoodCareer: true,
+    isGraduated: true,
+    isRespestableJob: true,
+    doPractice: "inFreeTime"
+
+
+}
+
+
+
+let coach = {
+
+    profession: "coach",
+    skills: "professionalInGame",
+
+    salaryInYear: 350000,
+    isGoodCareer: true,
+    work: [
+        "toDealsPlayer",
+        "toGiveOrder",
+    ]
+}
+
+
+
+let athleticTrainer = {
+
+    profession: "athleticTrainer",
+    training: "trainedToPlayers",
+    salaryInYear: 80000,
+    isGoodCareer: true,
+    scope: false,
+    famousUniversity: [
+        "sportsUniversity",
+
+
+    ]
+}
+let desginers = {
+
+    profession: "desginers",
+    work: [
+        "decoratRestuarants",
+        "decorateHomes",
+
+    ],
+    salaryInYear: 56000,
+    isGoodCareer: true,
+    famousUniversity: [
+        "artUniversity",
+        "federalUrduUniversityOfArts,SciencesAndTechnologyIslamabad.",
+
+    ]
+}
+
+
+let cameraMan = {
+
+    profession: "cameraMan",
+    work: "capturePics",
+    salaryInYear: 50000,
+    isGoodCareer: "mayBe",
+    ifExtraStudyFamousUniversity: undefined,
+}
+
+
+
+
+console.log(profession);
+console.log(doctor);
+console.log(physiotherapist);
+console.log(dermatology);
+console.log(diagnosticRadiology);
+console.log(anesthesiology);
+console.log(chemicalEngineer);
+console.log(mechanicalEngineer);
+console.log(civilEngineer);
+console.log(aeronauticalEngineer);
+console.log(electricalEngineer);
+console.log(accountant);
+console.log(fisherMan);
+console.log(teacher);
+console.log(architect);
+console.log(astronomers);
+console.log(softwareEngineer);
+console.log(pilot);
+console.log(fighterPilot);
+console.log(banker);
+console.log(lawyer);
+console.log(cheif);
+console.log(nurse);
+console.log(driver);
+console.log(painter);
+console.log(labour);
+console.log(businessMan);
+console.log(salesMan);
+console.log(scientist);
+console.log(police);
+console.log(magician);
+console.log(carpenter);
+console.log(swimmers);
+console.log(gardner);
+console.log(securityGaurd);
+console.log(importer);
+console.log(vascularSurgeons);
+console.log(pharmacist);
+console.log(transportationEngineer);
+console.log(agricultureEngineer);
+console.log(petroleumEngineer);
+console.log(exporter);
+console.log(artificialIntelligence);
+console.log(networking);
+console.log(graphics);
+console.log(Criminologists);
+console.log(skinSurgeon);
+console.log(made);
+console.log(brainSurgeon);
+console.log(audiologist);
+console.log(guidanceCounsellor);
+console.log(dietitian);
+console.log(psychologists);
+console.log(sexologists);
+console.log(forestEngineers);
+console.log(postman);
+console.log(fashionDesinger);
+console.log(mechanic);
+console.log(cashier);
+console.log(cricketer);
+console.log(footBaller);
+console.log(coach);
+console.log(athleticTrainer);
+console.log(desginers);
+console.log(cameraMan);
